f03831f0faa738a3271a61aea8bfbf3efba34768
glywvut <kirill.peshekhonov@mail.ru>
2025-05-12 13:25:55 +0300

    first commit

---
+++ include/AVLtree.h
@@ -0,0 +1,162 @@
+﻿#include<iostream>
+
+using namespace std;
+
+template <typename T>
+class AVL_Tree
+{
+public:
+    class Node
+    {
+    public:
+        T value;
+        int height;
+        Node* left;
+        Node* right;
+
+        Node(const T& val, const int& h = 0) : value(val), height(h), left(nullptr), right(nullptr) {}
+    };
+
+    AVL_Tree() : root(nullptr) {}
+
+    bool search(Node* node, const T val) const
+    {
+        if (node == nullptr) return false;
+        if (node->value == val) return true;
+        if (val > node->value) return search(node->right, val);
+        else return search(node->left, val);
+    }
+
+    void updateHeight(Node* node)
+    {
+        if (node == nullptr) return;
+        node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
+    }
+
+    int getHeight(Node* node) const
+    {
+        return (node == nullptr) ? -1 : node->height;
+    }
+
+    int getBalance(Node* node) const
+    {
+        return (node == nullptr) ? 0 : (getHeight(node->right) - getHeight(node->left));
+    }
+
+    void swapNodes(Node* a, Node* b)
+    {
+        T tmp = a->value;
+        a->value = b->value;
+        b->value = tmp;
+    }
+
+    void rightRotate(Node* node)
+    {
+        swapNodes(node, node->left);
+        Node* buffer = node->right;
+        node->right = node->left;
+        node->left = node->right->left;
+        node->right->left = node->right->right;
+        node->right->right = buffer;
+        updateHeight(node->right);
+        updateHeight(node);
+    }
+
+    void leftRotate(Node* node)
+    {
+        swapNodes(node, node->right);
+        Node* buffer = node->left;
+        node->left = node->right;
+        node->right = node->left->right;
+        node->left->right = node->left->left;
+        node->left->left = buffer;
+        updateHeight(node->left);
+        updateHeight(node);
+    }
+
+    void makeBalance(Node* node)
+    {
+        int balance = getBalance(node);
+        if (balance == -2)
+        {
+            if (getBalance(node->left) == 1) leftRotate(node->left);
+            rightRotate(node);
+        }
+        else if (balance == 2)
+        {
+            if (getBalance(node->right) == -1) rightRotate(node->right);
+            leftRotate(node);
+        }
+    }
+
+    bool insert(Node*& node, const T val)
+    {
+        if (node == nullptr)
+        {
+            node = new Node(val);
+            return true;
+        }
+
+        bool result = false;
+        if (val < node->value)
+        {
+            result = insert(node->left, val);
+        }
+        else if (val > node->value)
+        {
+            result = insert(node->right, val);
+        }
+        else
+        {
+            return false; 
+        }
+
+        updateHeight(node);
+        makeBalance(node);
+        return result;
+    }
+
+    Node* findMin(Node* node)
+    {
+        while (node->left != nullptr) node = node->left;
+        return node;
+    }
+
+    Node* erase(Node* node, const T val)
+    {
+        if (node == nullptr) return nullptr;
+
+        if (val < node->value)
+        {
+            node->left = erase(node->left, val);
+        }
+        else if (val > node->value)
+        {
+            node->right = erase(node->right, val);
+        }
+        else
+        {
+            if (node->left == nullptr || node->right == nullptr)
+            {
+                Node* temp = (node->left == nullptr) ? node->right : node->left;
+                delete node;
+                return temp;
+            }
+            else
+            {
+                Node* minRight = findMin(node->right);
+                node->value = minRight->value;
+                node->right = erase(node->right, minRight->value);
+            }
+        }
+
+        if (node == nullptr) return nullptr;
+
+        updateHeight(node);
+        makeBalance(node);
+        return node;
+    }
+
+public:
+    Node* root;
+};
---
+++ test/test_tree.cpp
@@ -0,0 +1,93 @@
+﻿#include"AVLtree.h"
+#include<gtest.h>
+
+TEST(AVLTreeTest, can_insert_and_search) {
+    AVL_Tree<int> tree;
+    tree.insert(tree.root, 10);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 5);
+    tree.insert(tree.root, 15);
+
+    EXPECT_TRUE(tree.search(tree.root, 10));
+    EXPECT_TRUE(tree.search(tree.root, 20));
+    EXPECT_TRUE(tree.search(tree.root, 5));
+    EXPECT_TRUE(tree.search(tree.root, 15));
+    EXPECT_FALSE(tree.search(tree.root, 25));
+}
+
+TEST(AVLTreeTest, insert_duplicates) {
+    AVL_Tree<int> tree;
+    EXPECT_TRUE(tree.insert(tree.root, 10));
+    EXPECT_FALSE(tree.insert(tree.root, 10)); 
+}
+
+TEST(AVLTreeTest, _erase_leaf_node) {
+    AVL_Tree<int> tree;
+    tree.insert(tree.root, 10);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 5);
+
+    tree.erase(tree.root, 5);
+    EXPECT_FALSE(tree.search(tree.root, 5));
+    EXPECT_TRUE(tree.search(tree.root, 10));
+    EXPECT_TRUE(tree.search(tree.root, 20));
+}
+
+TEST(AVLTreeTest, erase_node_with_one_child) {
+    AVL_Tree<int> tree;
+    tree.insert(tree.root, 10);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 15);
+    tree.insert(tree.root, 25);
+
+    tree.erase(tree.root, 20);
+    EXPECT_FALSE(tree.search(tree.root, 20));
+    EXPECT_TRUE(tree.search(tree.root, 15));
+    EXPECT_TRUE(tree.search(tree.root, 25));
+}
+
+TEST(AVLTreeTest, erase_node_with_two_children) {
+    AVL_Tree<int> tree;
+    tree.insert(tree.root, 10);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 5);
+    tree.insert(tree.root, 15);
+    tree.insert(tree.root, 25);
+    tree.insert(tree.root, 12);
+    tree.insert(tree.root, 17);
+
+    tree.erase(tree.root, 20);
+    EXPECT_FALSE(tree.search(tree.root, 20));
+    EXPECT_TRUE(tree.search(tree.root, 15));
+    EXPECT_TRUE(tree.search(tree.root, 25));
+    EXPECT_TRUE(tree.search(tree.root, 12));
+    EXPECT_TRUE(tree.search(tree.root, 17));
+}
+
+TEST(AVLTreeTest, balance_after_insert) {
+    AVL_Tree<int> tree;
+    tree.insert(tree.root, 10);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 30);
+
+    EXPECT_EQ(tree.getHeight(tree.root), 1);
+    EXPECT_EQ(tree.root->value, 20);
+    EXPECT_EQ(tree.root->left->value, 10);
+    EXPECT_EQ(tree.root->right->value, 30);
+}
+
+TEST(AVLTreeTest, balance_after_erase) {
+    AVL_Tree<int> tree;
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 10);
+    tree.insert(tree.root, 30);
+    tree.insert(tree.root, 5);
+    tree.insert(tree.root, 15);
+    tree.insert(tree.root, 25);
+    tree.insert(tree.root, 35);
+
+    tree.erase(tree.root, 35);
+    tree.erase(tree.root, 25);
+
+    EXPECT_LE(abs(tree.getBalance(tree.root)), 1);
+}
